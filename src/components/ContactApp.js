4bd74ece6b9de698931a2a21c38f76e41636a262
silvianaim02 <silvianaim68@gmail.com>
2022-09-20 15:37:23 +0700

    register

--- src/components/ContactApp.js
+++ src/components/ContactApp.js
@@ -3,23 +3,52 @@ import { Route, Routes } from 'react-router-dom';
 import Navigation from './Navigation';
 import HomePage from '../pages/HomePage';
 import AddPage from '../pages/AddPage';
+import RegisterPage from '../pages/RegisterPage';
  
+class ContactApp extends React.Component {
+  constructor(props) {
+    super(props);
  
-function ContactApp() {
-  return (
-    <div className="contact-app">
-      <header className='contact-app__header'>
-        <h1>Aplikasi Kontak</h1>
-        <Navigation />
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/add" element={<AddPage />} />
-        </Routes>
-      </main>
-    </div>
-  );
+    this.state = {
+      authedUser: null,
+    };
+  }
+
+  render() {
+    // not user
+    if (this.state.authedUser === null) {
+      return (
+        <div className='contact-app'>
+          <header className='contact-app__header'>
+            <h1>Aplikasi Kontak</h1>
+          </header>
+          <main>
+            <Routes>
+              {/* <Route path="/" element={<HomePage />} /> */}
+              <Route path="/*" element={<p>Halaman Login</p>} />
+              <Route path="/register" element={<RegisterPage />} />
+            </Routes>
+          </main>
+        </div>
+      )
+    }
+
+    // user
+    return (
+      <div className="contact-app">
+        <header className='contact-app__header'>
+          <h1>Aplikasi Kontak</h1>
+          <Navigation />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/add" element={<AddPage />} />
+          </Routes>
+        </main>
+      </div>
+    );
+  }
 }
  
 export default ContactApp;
